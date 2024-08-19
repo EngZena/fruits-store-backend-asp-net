47c917f3cfdc4361370b5602d6526177fb541be8
engZena <zena_al_ja95@yahoo.com>
2024-08-19 20:04:40 +0300

    Register API

--- Controller/AuthController.cs
+++ Controller/AuthController.cs
@@ -1,9 +1,11 @@
+using System.Data;
 using System.Security.Cryptography;
 using FruitsStoreBackendASPNET.Data;
 using FruitsStoreBackendASPNET.Dtos;
 using FruitsStoreBackendASPNET.Helpers;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace FruitsStoreBackendASPNET.Controllers
 {
@@ -24,8 +26,15 @@ namespace FruitsStoreBackendASPNET.Controllers
 
             if (userForRegistrationDto.Password == userForRegistrationDto.PasswordConfirm)
             {
-                string SQlCheckIfTheUserExists = "Select Email from FruitsStoreBackendSchema.Auth WHERE Email  = '" + userForRegistrationDto.Email + "'";
-                IEnumerable<string> existingUsers = _dapper.LoadData<string>(SQlCheckIfTheUserExists);
+                string SQlCheckIfTheUserExists =
+                    "Select Email from FruitsStoreBackendSchema.Auth WHERE Email  = '"
+                    + userForRegistrationDto.Email
+                    + "'";
+                IEnumerable<string> existingUsers = _dapper.LoadData<string>(
+                    SQlCheckIfTheUserExists
+                );
+
+                // Verify whether the email address is registered previously
 
                 if (existingUsers.Count() == 0)
                 {
@@ -35,32 +44,67 @@ namespace FruitsStoreBackendASPNET.Controllers
                         rng.GetNonZeroBytes(passwordSalt);
                     }
 
-                    byte[] passwordHash = _authHelper.GetPasswordHash(userForRegistrationDto.Password, passwordSalt);
+                    byte[] passwordHash = _authHelper.GetPasswordHash(
+                        userForRegistrationDto.Password,
+                        passwordSalt
+                    );
 
-                    string SQLAddAUser = @"INSERT INTO FruitsStoreBackendSchema.Users(
+                    string sqlAddAuth =
+                        @"INSERT INTO FruitsStoreBackendSchema.AUTH ([Email],
+                                        [passwordHash],
+                                        [passwordSalt]) VALUES('"
+                        + userForRegistrationDto.Email
+                        + "', @passwordHash, @passwordSalt)";
+
+                    List<SqlParameter> SqlParameters = new List<SqlParameter>();
+
+                    SqlParameter passwordSaltParameter = new SqlParameter(
+                        "@PasswordSalt",
+                        SqlDbType.VarBinary
+                    );
+                    passwordSaltParameter.Value = passwordSalt;
+
+                    SqlParameter passwordHashParameter = new SqlParameter(
+                        "@PasswordHash",
+                        SqlDbType.VarBinary
+                    );
+                    passwordHashParameter.Value = passwordHash;
+
+                    SqlParameters.Add(passwordHashParameter);
+                    SqlParameters.Add(passwordSaltParameter);
+
+                    if (_dapper.ExecuteSqlWithListParameters(sqlAddAuth, SqlParameters))
+                    {
+                        string SQLAddAUser =
+                            @"INSERT INTO FruitsStoreBackendSchema.Users(
                                      [FirstName],   
                                      [LastName],   
                                      [Email],   
                                      [Gender],   
                                      [Active]
                                     ) VALUES("
-                                        + "'" + userForRegistrationDto.FirstName
-                                        + "',  '" + userForRegistrationDto.LastName
-                                        + "',  '" + userForRegistrationDto.Email
-                                        + "',  '" + userForRegistrationDto.Gender
-                                        + "' , 1)";
-                    if (_dapper.ExecuteSql(SQLAddAUser))
-                    {
-                        return Ok();
+                            + "'"
+                            + userForRegistrationDto.FirstName
+                            + "',  '"
+                            + userForRegistrationDto.LastName
+                            + "',  '"
+                            + userForRegistrationDto.Email
+                            + "',  '"
+                            + userForRegistrationDto.Gender
+                            + "' , 1)";
+                        if (_dapper.ExecuteSql(SQLAddAUser))
+                        {
+                            return Ok();
+                        }
+                        throw new Exception("Failed to Add User");
                     }
                     throw new Exception("Failed to Create User");
-
                 }
                 throw new Exception("There is already a user with this email.");
-
             }
-            throw new Exception("There is a mismatch between the password and the confirm passwords.");
-
+            throw new Exception(
+                "There is a mismatch between the password and the confirm passwords."
+            );
         }
     }
 }
