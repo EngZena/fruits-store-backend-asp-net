e14d9fd28ecbea5d8491f32674196f2866265fba
engZena <zena_al_ja95@yahoo.com>
2025-04-02 01:48:26 +0300

    add swagger description to the Data Transfer Object

--- Data/UserForLoginDto.cs
+++ Data/UserForLoginDto.cs
@@ -1,8 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FruitsStoreBackendASPNET.Dtos
 {
     public partial class UserForLoginDto
     {
+        /// <summary>
+        /// The email of the user
+        /// </summary>
+        /// <example>email@gmail.com</example>
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
         public string Email { get; set; }
+
+        /// <summary>
+        /// The password of the user
+        /// </summary>
+        [Required(ErrorMessage = "Password is required.")]
         public string Password { get; set; }
 
         public UserForLoginDto()
--- Dtos/UserForRegistrationDto.cs
+++ Dtos/UserForRegistrationDto.cs
@@ -5,22 +5,44 @@ namespace FruitsStoreBackendASPNET.Dtos
 {
     public partial class UserForRegistrationDto
     {
+        /// <summary>
+        /// The email of the user
+        /// </summary>
+        /// <example>email@gmail.com</example>
         [Required(ErrorMessage = "Email is required.")]
         [EmailAddress(ErrorMessage = "Invalid email format.")]
         public string Email { get; set; }
 
+        /// <summary>
+        /// The password of the user
+        /// </summary>
         [Required(ErrorMessage = "Password is required.")]
         public string Password { get; set; }
 
+        /// <summary>
+        /// The password confirm of the user
+        /// </summary>
         [Required(ErrorMessage = "PasswordConfirm is required.")]
         public string PasswordConfirm { get; set; }
 
+        /// <summary>
+        /// The first name of the user
+        /// </summary>
+        /// <example>name</example>
         [Required(ErrorMessage = "FirstName is required.")]
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// The last name of the user
+        /// </summary>
+        /// <example>name</example>
         [Required(ErrorMessage = "LastName is required.")]
         public string LastName { get; set; }
 
+        /// <summary>
+        /// The gender of the user
+        /// </summary>
+        /// <example>Female</example>
         [Required(ErrorMessage = "Gender is required.")]
         [EnumDataType(typeof(Gender), ErrorMessage = "Invalid gender value.")]
         public Gender Gender { get; set; }
