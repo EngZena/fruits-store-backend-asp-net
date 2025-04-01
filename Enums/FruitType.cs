8a4f2d68735550a0f37dcbfbbf6178d716993d44
engZena <zena_al_ja95@yahoo.com>
2025-04-02 01:50:35 +0300

    add swagger description to the Enums

--- Enums/FruitType.cs
+++ Enums/FruitType.cs
@@ -1,8 +1,22 @@
+using System.Text.Json.Serialization;
+
 namespace FruitsStoreBackendASPNET.Enums
 {
+    /// <summary>
+    /// Represents the type of the fruit.
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum FruitType
     {
+        /// <summary>
+        /// Winter Fruits (0).
+        /// </summary>
         WINTER_FRUITS,
+
+        /// <summary>
+        /// Summer Fruits (1).
+        /// </summary>
+
         SUMMER_FRUITS,
     }
 }
--- Enums/Gender.cs
+++ Enums/Gender.cs
@@ -1,8 +1,21 @@
+using System.Text.Json.Serialization;
+
 namespace FruitsStoreBackendASPNET.Enums
 {
+    /// <summary>
+    /// Represents the gender of a user.
+    /// </summary>
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum Gender
     {
-        Male,
-        Female,
+        /// <summary>
+        /// Male gender (0).
+        /// </summary>
+        Male = 0,
+
+        /// <summary>
+        /// Female gender (1).
+        /// </summary>
+        Female = 1,
     }
 }
